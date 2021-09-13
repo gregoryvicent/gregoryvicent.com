05bed682cf5003a223857c48fdc53f8330f71959
Gregory Vicent <gregory.work14@gmail.com>
2021-09-13 14:20:46 -0400

    Changes in navbar

--- components/nav.js
+++ components/nav.js
@@ -9,19 +9,19 @@ export default function Nav({ className }) {
       <Link href="/blog">
         <div className={ className } >
           <h4 className={ style.nav_title }>Blog &#8658;</h4>
-          <p className={ style.nav_text }>Visita mi blog, aquí divago y hablo de cosas que captan mi atención, tal vez algún post llame tu interés 😉</p>
+          <p className={ style.nav_text }>Visita el blog, aquí divago y hablo de cosas que captan mi atención, tal vez algún post llame tu interés 😉</p>
         </div>
       </Link>
       <Link href="/tutoriales">
         <div className={ className } >
           <h4 className={ style.nav_title }>Tutoriales &#8658;</h4>
-          <p className={ style.nav_text }>Algúnos tutoriales para poner en practica diversas tecnologías, ¡No hay mejor forma de aprender que enseñando 👨‍💻!</p>
+          <p className={ style.nav_text }>Algúnos tutoriales para dar respuesta a preguntas comunes y no tan comunes de diversas tecnologías 👨‍💻</p>
         </div>
       </Link>
       <Link href="/juguetes">
         <div className={ className } >
           <h4 className={ style.nav_title }>Juguetes &#8658;</h4>
-          <p className={ style.nav_text }>Mis trabajos, proyectos y juguetes más destacados referentes al mundo del código 👾</p>
+          <p className={ style.nav_text }>Algunos proyectos interesantes para poner manos sobre el código y crear cosas chulas 👾</p>
         </div>
       </Link>
     </nav>
