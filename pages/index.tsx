4d51834354c74a82238a913e175c89424b7d1c39
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-04-18 07:57:20 -0400

    003: Added dir styles/ and new css files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
+import styles from '../styles/index.module.css'
+
 export default function Home() {
   return (
     <main>
       <Head>
 	<title>Gregory Vicent</title>
       </Head>
-      <h1>GregoryVicent.com</h1>
+      <h1 className={styles.title} >GregoryVicent.com</h1>
       <Link href="/posts/postOne">First Post</Link>
     </main>
   )
